9dff8530b8ec4b6026fcd929522d2e76138d19f3
mozhdeh <59051586+mozhdehNouri@users.noreply.github.com>
2023-11-21 12:44:19 +0330

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,6 @@
 buildscript {
     repositories {
-        maven {
-            url = uri("https://nexus.partdp.ir/repository/part-android/")
-            artifactUrls("https://nexus.partdp.ir/repository/part-android/")
-            credentials {
-                username = "android-user"
-                password = "EL2BB+*wkXEaydY=/2>2Kx-tV4CV-%"
-            }
-        }
+    
     }
 }
 
