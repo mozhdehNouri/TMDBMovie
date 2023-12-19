cff2707251990ca586b99c872a1fe7c7d0ec0ae5
mozhdeh.nouri <mozhdehnouri@yahoo.com>
2023-12-19 16:51:01 +0330

    Update Dependencies

--- build-logic/convention/src/main/java/TMDBFlavor.kt
+++ build-logic/convention/src/main/java/TMDBFlavor.kt
@@ -19,7 +19,7 @@ enum class NiaFlavor(val dimension: FlavorDimension, val applicationIdSuffix: St
 }
 
 fun Project.configureFlavors(
-    commonExtension: CommonExtension<*, *, *, *>,
+    commonExtension: CommonExtension<*, *, *, *, *>,
     flavorConfigurationBlock: ProductFlavor.(flavor: NiaFlavor) -> Unit = {}
 ) {
     commonExtension.apply {
