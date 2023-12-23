12a4f2441cd9b7f285e42026ce82965fc16c24a9
mozhdeh.nouri <mozhdehnouri@yahoo.com>
2023-12-23 14:42:14 +0330

    Update Dependencies (#Master)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,27 +1,11 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
-        maven {
-            url = uri("https://nexus.partdp.ir/repository/part-android/")
-            artifactUrls("https://nexus.partdp.ir/repository/part-android/")
-            credentials {
-                username = "android-user"
-                password = "EL2BB+*wkXEaydY=/2>2Kx-tV4CV-%"
-            }
-        }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven {
-            url = uri("https://nexus.partdp.ir/repository/part-android/")
-            artifactUrls("https://nexus.partdp.ir/repository/part-android/")
-            credentials {
-                username = "android-user"
-                password = "EL2BB+*wkXEaydY=/2>2Kx-tV4CV-%"
-            }
-        }
     }
 }
 rootProject.name = "MovieTMDB"
